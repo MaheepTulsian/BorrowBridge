57a003d94a47a33006fafaeb4e26c41ff4b7534d
Contactsushil <contact.sushilpandey@gmail.com>
2024-01-23 01:59:40 +0530

    Basic Input box

--- BBfront/src/Pages/Oppurtunity.jsx
+++ BBfront/src/Pages/Oppurtunity.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import { useParams } from 'react-router-dom';
+import Right from "../Components/Right"
+import Left from '../Components/Left';
+import './Page.css'
 
 const Oppurtunity = () => {
     const { id } = useParams();
   return (
-    <div>Oppurtunity {id}</div>
+    <>
+    <div className="boxforopp">
+      <div className="boxopri">
+        <Right/>
+      </div>
+      <div className="boxople">
+        <Left/>
+      </div>
+    </div>
+    </>
   )
 }
 
