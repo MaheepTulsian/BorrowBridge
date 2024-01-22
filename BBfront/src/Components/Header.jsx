0d997e6aeba8f1e7c5bf5a65c0cd3732b47ec227
Ashish_007 <128707638+ashish787898@users.noreply.github.com>
2024-01-23 02:33:06 +0530

    nav bar

--- BBfront/src/Components/Header.jsx
+++ BBfront/src/Components/Header.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import './Comp.css';
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="container nav_container">
+      <a href="index.html">
+        <h3>LOGO</h3>
+      </a>
+      <ul className="nav_menu_horizontal">
+        <li><a href="index.html">Home</a></li>
+        <li><a href="about.html">About</a></li>
+        <li><a href="course.html">Course</a></li>
+        <li><a href="contact.html">Contact</a></li>
+        <li className="contact_button"><button>Contact Us</button></li>
+      </ul>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
