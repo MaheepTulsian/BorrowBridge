f6c7c07b09407b24facb7751831f99cfe537fad4
Satyam Kumar <sk9161511745@gmail.com>
2024-01-23 12:22:31 +0530

    added cors

--- server/api/opp_api.js
+++ server/api/opp_api.js
@@ -2,8 +2,9 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import db from "../Database/connect.js";
-
+import User from '../../server/Database/userschema.js';
 import Oppo from "../../server/Database/oppurtunity.js";
+
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,6 +12,7 @@ const port = process.env.PORT || 3000;
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("I am live");
