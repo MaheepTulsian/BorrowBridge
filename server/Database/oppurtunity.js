6954af4e14c749d2c8b301c0493e1bce764b02c0
singhsatyendra8052 <singhsatyendra8052@gmail.com>
2024-01-23 14:18:04 +0530

    update
    update

--- server/Database/oppurtunity.js
+++ server/Database/oppurtunity.js
@@ -1,60 +1,72 @@
 import mongoose from "mongoose";
 
-const oppo_schema = mongoose.Schema({
-  oppurtunity_id: {
-    type: String,
-    required: true,
-  },
-  title: {
-    type: String,
-    required: true,
-  },
-  Thumbnail: {
-    type: String,
-    required: true,
-  },
-  Description: {
-    type: String,
-    required: true,
-  },
-  contact_Details: {
-    type: String,
-    required: true,
-  },
-  pitch_Pdf: {
-    type: String,
-    required: true,
-  },
-  token_Requested: {
-    type: Number,
-    required: true,
-  },
-  Location: {
-    type: String,
-    required: true,
-  },
+const oppo_schema = mongoose.Schema(
+  {
+    oppurtunity_id: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    Thumbnail: {
+      type: String,
+      required: true,
+    },
+    Description: {
+      type: String,
+      required: true,
+    },
+    contact_Details: {
+      type: String,
+      required: true,
+    },
+    pitch_Pdf: {
+      type: String,
+      required: true,
+    },
+    token_Requested: {
+      type: Number,
+      required: true,
+    },
+    Location: {
+      type: String,
+      required: true,
+    },
+    Amount: {
+      type: Number,
+    },
+    Interest: {
+      type: Number,
+    },
+    amount_to_be_retured: {
+      type: Number,
+    },
+    duration_of_time: {
+      type: Number,
+    },
 
-  Amount_Collected: {
-    type: Number,
-    required: true,
+    Amount_Collected: {
+      type: Number,
+    },
+
+    Investors: [
+      {
+        wallet_id: {
+          type: String,
+        },
+        Amount: {
+          type: Number,
+        },
+      },
+    ],
+    Status: [
+      {
+        type: String,
+      },
+    ],
   },
-  Investors:[
-    {
-        wallet_id:{
-            type: String,
-            required: true
-        },
-        Amount:{
-            type: Number,
-            required: true
-        },
-    },  
-  ],
-  Status: [
-    {
-        type: String
-    }
-  ]},
   {
     timestamps: true,
   }
